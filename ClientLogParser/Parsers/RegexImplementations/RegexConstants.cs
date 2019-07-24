c71f6988e3f5dc924fe9ff1408d188c508012fed
mztikk <nevigskcuforez@gmail.com>
2019-07-24 14:15:37 +0200

    added TierRegex to constants

--- ClientLogParser/Parsers/RegexImplementations/RegexConstants.cs
+++ ClientLogParser/Parsers/RegexImplementations/RegexConstants.cs
@@ -14,6 +14,7 @@ namespace ClientLogParser.Parsers.RegexImplementations
         internal const string _LeagueNameRegex = @"([\p{L}\p{N}\p{P}\p{S}\p{Z}]+?)";
         internal const string _PositionRegex = @"left ([\d]+?), top ([\d]+?)";
         internal const string _AreaRegex = @"([\p{L}\p{N}\p{P}\p{S}\p{Z}][^\.]+)";
+        internal const string _TierRegex = @"T(\d*)";
 
         internal static readonly Regex MapPattern = new Regex(@"(.*) \(T(\d*)\)", RegexOptions.Compiled);
         internal static readonly Regex OffiGemPattern = new Regex(@"(.*)level (\d*) (\d*)% (.*)", RegexOptions.Compiled);
